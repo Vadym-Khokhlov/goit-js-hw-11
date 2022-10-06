bd32193e9e51dd75397a3b69aba564edc23b079d
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2022-10-06 19:30:55 +0300

    2 of 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,25 @@
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
+const START_DELAY = 1000;
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
-const PROMPT_DELAY = 1000;
 let intervalId = null;
 
-startBtn.addEventListener('click', () => {
+function startClicker() {
   intervalId = setInterval(() => {
     const color = getRandomHexColor();
     body.style.backgroundColor = color;
-  }, PROMPT_DELAY);
-});
+  }, START_DELAY);
+  startButton.disabled = true;
+}
 
-stopBtn.addEventListener('click', () => {
+function stopClicker() {
   clearInterval(intervalId);
-});
+  startButton.disabled = false;
+}
+
+startButton.addEventListener('click', startClicker);
+stopButton.addEventListener('click', stopClicker);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-// need button disabled
