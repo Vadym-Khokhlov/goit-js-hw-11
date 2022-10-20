ffdc5d6f8df54ee876625b17df36fd90484fa327
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2022-10-20 22:48:27 +0300

    imports and markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-//
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const url = 'https://pixabay.com/api/';
+const params = {
+  key: '30575180-f51bf292afceb69c3d087b7fc',
+  orientation: 'horizontal',
+  q: '',
+  image_type: 'photo',
+  safesearch: true,
+};
+
+const refs = {
+  searchForm: document.getElementById('search-form'),
+  gallery: document.querySelector('.gallery'),
+  buttonLoad: document.querySelector('.load-more'),
+};
